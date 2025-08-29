ea00682261b20f28ca9f6fe74c9ad349eea6e87c
Soiidux <soiidux040@gmail.com>
2025-08-29 18:42:09 +0530

    Building: Making webpages and routes

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { VertexAI } from '@google-cloud/vertexai';
+import { dbConnect } from './db/dbConnect.js';
+import router from './routes/user.routes.js';
 
 //loading configs
 dotenv.config();
 const PROJECT_ID = process.env.PROJECT_ID;
 const LOCATION = process.env.LOCATION;
 const MODEL_NAME = process.env.MODEL_NAME;
-const GOOGLE_APPLICATION_CREDENTIALS = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+// const GOOGLE_APPLICATION_CREDENTIALS = process.env.GOOGLE_APPLICATION_CREDENTIALS;
 const PORT = process.env.PORT;
 
 //initializing the app
 const app = express();
 app.use(express.json());     //json built-in middleware
+app.use(router);
 
 //initializing vertexAI
 const vertex_ai = new VertexAI({ project: PROJECT_ID, location: LOCATION });
@@ -51,6 +54,12 @@ app.post('/get-advice', async (req, res) => {
 });
 
 
-app.listen(PORT,()=>{
+dbConnect()
+  .then(()=>{
+    app.listen(PORT,()=>{
     console.log("Server listening at port: ",PORT);
+    })
+  })
+  .catch((error)=>{
+    console.log(`Mongodb connection error: ${error}`)
 })
